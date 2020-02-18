e133a94422f36b06641d03ba26e0918f8a576698
Katherine Langley <promoteglobal@gmail.com>
2020-02-18 10:56:07 +0800

    Created color functioning

--- designs.js
+++ designs.js
@@ -1,4 +1,26 @@
 // Select color input
+let colorPicked;
+
+function selectColor (event) {
+  colorPicked = event.target.value;
+}
+
+function changeColor (event) {
+  event.target.style.color = colorPicked;
+}
+
+
+const colorEl = document.getElementById("colorPicker");
+colorEl.addEventListener("change", selectColor);
+
+const p1 = document.querySelector("#one");
+p1.addEventListener("click", changeColor);
+
+const p2 = document.querySelector("#two");
+p2.addEventListener("click", changeColor);
+
+
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
